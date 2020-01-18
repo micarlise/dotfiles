e97b8664108e1cf2210c0876ec62ddcc7022be81
M Carlise <micarlise@gmail.com>
2020-01-18 14:39:24 -0500

    init-functions: allow functions to modify any passed in variable
    Initially, init-functions only update/modify the PATH variable.
    There are a number of other variables we want to control to
    enable/disable dev environments: LD_LIBRARY_PATH, PKG_CONFIG_PATH,
    MANPATH to name a few.

--- bash/init-functions.bash
+++ bash/init-functions.bash
@@ -1,59 +1,78 @@
 
 function addpathf {
+    # Add path segment to front of PATH like variable
+
+    declare -n REFPATH=$1
+    shift
+
     for x in ${1//:/ }; do
-		case ":$PATH:" in
-            *":${x}:"*) ;; # already in PATH
-			*) PATH="${x}:$PATH" ;;
+		case ":$REFREFPATH:" in
+            *":${x}:"*) ;; # already in REFPATH
+			*) REFPATH="${x}:$REFPATH" ;;
 		esac
 	done
     unset x 
 }
 
 function addpathr {
+    # Add path segment to end of PATH like variable
+    declare -n REFPATH=$1
+    shift
+
     for x in ${1//:/ }; do
-		case ":$PATH:" in
-            *":${x}:"*) ;; # already in PATH
-            *) PATH="${PATH%:}:${x%:}" ;;
+		case ":$REFPATH:" in
+            *":${x}:"*) ;; # already in REFPATH
+            *) REFPATH="${REFPATH%:}:${x%:}" ;;
 		esac
 	done
     unset x 
 }
 
 function removepath {
-    if [ -n "${PATH}" ]; then
-        local segments=(${PATH//:/ })
+    # remove path segment from PATH like variable
+
+    declare -n REFPATH=$1
+    shift
+
+    if [ -n "${REFPATH}" ]; then
+        local segments=(${REFPATH//:/ })
 
         local entries=()
         for ((i="${#segments[@]}"-1;i>=0;i--)); do
             entries+=("${segments[i]}");
         done
 
-        PATH=
+        REFPATH=
         for entry in "${entries[@]}"; do
             case ":${1}:" in
                 *":${entry}:"*) ;; # entry labeled for removal
                 *) addpathf ${entry} ;; # add everything else
             esac
         done
-        PATH=${PATH%:} 
+        REFPATH=${REFPATH%:} 
         unset entry 
     fi
 }
 
 function cleanpath {
-	if [ -n "${PATH}" ]; then
-        local segments=(${PATH//:/ }) 
+    # remove duplicates from PATH like variable
+
+    declare -n REFPATH=$1
+    shift
+
+	if [ -n "${REFPATH}" ]; then
+        local segments=(${REFPATH//:/ }) 
 
         local entries=()
         for ((i="${#segments[@]}"-1;i>=0;i--)); do
             entries+=("${segments[i]}");
         done
 
-        PATH=
+        REFPATH=
         for entry in "${entries[@]}"; do
             addpathf ${entry}
         done
-        PATH=${PATH%:}
+        REFPATH=${REFPATH%:}
         unset entry 
     fi
 }
